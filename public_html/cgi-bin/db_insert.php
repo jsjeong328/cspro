71764d0a12832be3c031251985e68a787b4e47d9
jsjeong328 <jsjeong328@gmail.com>
2017-11-05 22:15:16 +0900

    17

--- public_html/cgi-bin/db_insert.php
+++ public_html/cgi-bin/db_insert.php
@@ -5,7 +5,7 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-echo ("Order Complete!<br/>");
+
 $hostname = "localhost";
 $username = "cs20101697";
 $password = "dbpass";
@@ -19,9 +19,6 @@ function validation_check($data){
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
-  if($data==null){//만약 잘못된 데이터 들어왔다면 에러페이지로
-    return false;
-  }
   return $data;
 }
 
@@ -34,20 +31,25 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
   $paymethod = validation_check($_POST["paymethod"]);
   $callfirst = validation_check($_POST["callfirst"]);
 }
+if(is_numeric($_POST["idnumber"])){
+  $stmt = $connect->prepare('INSERT INTO `pizza_order` (id_number, name, email, phone_number, topping, pay_method, call_first, order_date)
+  VALUES(:idnumber, :name, :email, :phone, :topping, :paymethod, :callfirst, NOW())');
 
-$stmt = $connect->prepare('INSERT INTO `pizza_order` (id_number, name, email, phone_number, topping, pay_method, call_first, order_date)
-VALUES(:idnumber, :name, :email, :phone, :topping, :paymethod, :callfirst, NOW())');
+  $stmt->bindParam(':idnumber', $idnumber, PDO::PARAM_INT);
+  $stmt->bindParam(':name', $name, PDO::PARAM_STR, 30);
+  $stmt->bindParam(':email', $email, PDO::PARAM_STR, 30);
+  $stmt->bindParam(':phone', $phone, PDO::PARAM_STR, 30);
+  $stmt->bindParam(':topping', $topping, PDO::PARAM_STR, 30);
+  $stmt->bindParam(':paymethod', $paymethod, PDO::PARAM_STR, 30);
+  $stmt->bindParam(':callfirst', $callfirst, PDO::PARAM_STR, 30);
 
-$stmt->bindParam(':idnumber', $idnumber, PDO::PARAM_INT);
-$stmt->bindParam(':name', $name, PDO::PARAM_STR, 30);
-$stmt->bindParam(':email', $email, PDO::PARAM_STR, 30);
-$stmt->bindParam(':phone', $phone, PDO::PARAM_STR, 30);
-$stmt->bindParam(':topping', $topping, PDO::PARAM_STR, 30);
-$stmt->bindParam(':paymethod', $paymethod, PDO::PARAM_STR, 30);
-$stmt->bindParam(':callfirst', $callfirst, PDO::PARAM_STR, 30);
+  $stmt->execute();
+  $connect=null;
+  echo ("Order Complete!<br/>");
+}else{
+  echo ("ID number must be numbers");
+}
 
-$stmt->execute();
-$connect=null;
 ?>
 
 </body>
