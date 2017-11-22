046460995ce1a15af9c6839461666c325609d0d2
jsjeong328 <jsjeong328@gmail.com>
2017-11-22 19:02:50 +0900

    1321231

--- public_html/searchAjax.js
+++ public_html/searchAjax.js
@@ -1,25 +1,25 @@
 var runAutoComplete = (function(){
   var ajaxUrl = "http://cspro.sogang.ac.kr/~cse20101697/cgi-bin/db_display.php";
 
-  // $("#order_number").autocomplete({
-  //   source: function(request, response){
-  //     $.ajax({
-  //         type: 'get'
-  //       , url: ajaxUrl
-  //       , dataType: "json"
-  //       , data: {"order_number" : request.term}
-  //       , success: function(data) {
-  //         response( $.map( data, function( item ) {
-  //           return {
-  //             label: item,
-  //             value: item
-  //           }
-  //         }));
-  //       }
-  //     });
-  //   },
-  //   minLength: 2
-  // });
+  $("#order_number").autocomplete({
+    source: function(request, response){
+      $.ajax({
+          type: 'get'
+        , url: ajaxUrl
+        , dataType: "json"
+        , data: {"order_number" : request.term}
+        , success: function(data) {
+          response( $.map( data, function( item ) {
+            return {
+              label: item,
+              value: item
+            }
+          }));
+        }
+      });
+    },
+    minLength: 2
+  });
 
   $("#id_number").autocomplete({
     source: function(request, response){
