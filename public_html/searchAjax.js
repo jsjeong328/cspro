6c9745e52751cea66965be32f7123e1076b956f0
jsjeong328 <jsjeong328@gmail.com>
2017-11-22 16:53:32 +0900

    ajax frame

---
+++ public_html/searchAjax.js
@@ -0,0 +1,21 @@
+var runAutoComplete = (function(){
+  var ajaxUrl = "http://cspro.sogang.ac.kr/~cse20101697/cgi-bin/db_display.php";
+
+  $("#order_number").autocomplete({
+    source: function(request, response){
+      $.ajax({
+          type: 'get'
+        , url: ajaxUrl
+        , dataType: "json"
+        , data: {"order_number" : 1}
+        , success: function(data){
+          console.log(data);
+        }
+      });
+    },
+    minLength: 2,
+    select: function(event, ui){
+
+    }
+  });
+}());
