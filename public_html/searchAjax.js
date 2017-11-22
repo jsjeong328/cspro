011515854592c042afe672b820e172b1948d82d8
jsjeong328 <jsjeong328@gmail.com>
2017-11-22 17:20:19 +0900

    php 6

--- public_html/searchAjax.js
+++ public_html/searchAjax.js
@@ -8,18 +8,9 @@ var runAutoComplete = (function(){
         , url: ajaxUrl
         , dataType: "json"
         , data: {"order_number" : 1}
-        ,  success: function(data) {
-                        //서버에서 json 데이터 response 후 목록에 뿌려주기 위함
-                        response(
-                            $.map(data, function(item) {
-                                return {
-                                    label: item.data,
-                                    value: item.data
-                                }
-                            })
-                        );
-                    }
-
+        , success: function(data) {
+          return data;
+        }
       });
     },
     minLength: 2,
