9e470d201f4e1ba5c04982d19ad2fa10d6f730b7
jsjeong328 <jsjeong328@gmail.com>
2017-11-22 18:21:26 +0900

    final 2

--- public_html/cgi-bin/db_display.php
+++ public_html/cgi-bin/db_display.php
@@ -10,43 +10,43 @@ $dbname = "db20101697";
 $connect = new PDO("mysql:dbname=$dbname;host=$hostname", $username, $password) or die("DB Connection Failed");
 
 if(isset($_GET["order_number"])){
-  $order_number = $_GET["order_number"];
-  $stmt = $connect->prepare('select * from pizza_order WHERE `order_number` = :order_number');
+  $order_number = '%'.$_GET["order_number"].'%';
+  $stmt = $connect->prepare('select * from pizza_order WHERE `order_number` LIKE :order_number');
   $stmt->bindParam(':order_number', $order_number, PDO::PARAM_INT);
 }
 if(isset($_GET["id_number"])){
-  $id_number = $_GET["id_number"];
-  $stmt = $connect->prepare('select * from pizza_order WHERE `id_number` = :id_number');
+  $id_number = '%'.$_GET["id_number"].'%';
+  $stmt = $connect->prepare('select * from pizza_order WHERE `id_number` LIKE :id_number');
   $stmt->bindParam(':id_number', $id_number, PDO::PARAM_INT);
 }
 if(isset($_GET["name"])){
-  $name = $_GET["name"];
-  $stmt = $connect->prepare('select * from pizza_order WHERE `name` = :name');
+  $name = '%'.$_GET["name"].'%';
+  $stmt = $connect->prepare('select * from pizza_order WHERE `name` LIKE :name');
   $stmt->bindParam(':name', $name, PDO::PARAM_STR, 30);
 }
 if(isset($_GET["email"])){
-  $email = $_GET["email"];
-  $stmt = $connect->prepare('select * from pizza_order WHERE `email` = :email');
+  $email = '%'.$_GET["email"].'%';
+  $stmt = $connect->prepare('select * from pizza_order WHERE `email` LIKE :email');
   $stmt->bindParam(':email', $email, PDO::PARAM_STR, 30);
 }
 if(isset($_GET["phone_number"])){
-  $phone_number = $_GET["phone_number"];
-  $stmt = $connect->prepare('select * from pizza_order WHERE `phone_number` = :phone_number');
+  $phone_number = '%'.$_GET["phone_number"].'%';
+  $stmt = $connect->prepare('select * from pizza_order WHERE `phone_number` LIKE :phone_number');
   $stmt->bindParam(':phone_number', $phone_number, PDO::PARAM_STR, 30);
 }
 if(isset($_GET["topping"])){
-  $topping = $_GET["topping"];
-  $stmt = $connect->prepare('select * from pizza_order WHERE `topping` = :topping');
+  $topping = '%'.$_GET["topping"].'%';
+  $stmt = $connect->prepare('select * from pizza_order WHERE `topping` LIKE :topping');
   $stmt->bindParam(':topping', $topping, PDO::PARAM_STR, 30);
 }
 if(isset($_GET["pay_method"])){
-  $pay_method = $_GET["pay_method"];
-  $stmt = $connect->prepare('select * from pizza_order WHERE `pay_method` = :pay_method');
+  $pay_method = '%'.$_GET["pay_method"].'%';
+  $stmt = $connect->prepare('select * from pizza_order WHERE `pay_method` LIKE :pay_method');
   $stmt->bindParam(':pay_method', $pay_method, PDO::PARAM_STR, 30);
 }
 if(isset($_GET["call_first"])){
-  $call_first = $_GET["call_first"];
-  $stmt = $connect->prepare('select * from pizza_order WHERE `call_first` = :call_first');
+  $call_first = '%'.$_GET["call_first"].'%';
+  $stmt = $connect->prepare('select * from pizza_order WHERE `call_first` LIKE :call_first');
   $stmt->bindParam(':call_first', $call_first, PDO::PARAM_STR, 30);
 }
 if(isset($_GET["order_date"])){
