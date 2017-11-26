9ddcfa1bab1b9bcf6d5b5460b888f7dc4c202feb
jsjeong328 <jsjeong328@gmail.com>
2017-11-26 16:02:31 +0900

    test11

--- public_html/searchAjax.js
+++ public_html/searchAjax.js
@@ -1,39 +1,38 @@
 var runAutoComplete = (function(){
-  console.log(1)
-  // var ajaxUrl = "http://cspro.sogang.ac.kr/~cse20101697/cgi-bin/db_display.php";
-  // var str = "";
-  // var $opt = "";
-  // $( "select" ).on('change', function() {
-  //   $( "select option:selected" ).each(function() {
-  //     $opt = $(this).text();
-  //     str +=
-  //        '<div class="ui-widget">'
-  //         +'<label for="'+$opt+'">find by '+$opt+': </label>'
-  //         +'<input id="'+$opt+'">'
-  //       +'</div>'
-  //       +'<HR>';
-  //   });
-  //   $("#selectbox").html( str );
-  //
-  //   $("#"+$opt+"").autocomplete({
-  //     source: function(request, response){
-  //       $.ajax({
-  //           type: 'get'
-  //         , url: ajaxUrl
-  //         , dataType: "json"
-  //         , data: {$opt : request.term}
-  //         , success: function(data) {
-  //           response( $.map( data, function( item ) {
-  //             var toShow = item.order_number + ' '+ item.id_number + ' '+ item.name + ' '+ item.email + ' '+ item.phone_number + ' '+ item.topping + ' '+ item.pay_method + ' '+ item.call_first + ' '+ item.order_date;
-  //             return {
-  //               label: toShow,
-  //               value: toShow
-  //             }
-  //           }));
-  //         }
-  //       });
-  //     },
-  //     minLength: 2
-  //   });
-  // }).trigger( "change" );
+  var ajaxUrl = "http://cspro.sogang.ac.kr/~cse20101697/cgi-bin/db_display.php";
+  var str = "";
+  var $opt = "";
+  $( "select" ).on('change', function() {
+    $( "select option:selected" ).each(function() {
+      $opt = $(this).text();
+      str +=
+         '<div class="ui-widget">'
+          +'<label for="'+$opt+'">find by '+$opt+': </label>'
+          +'<input id="'+$opt+'">'
+        +'</div>'
+        +'<HR>';
+    });
+    $("#selectbox").html( str );
+
+    $("#"+$opt+"").autocomplete({
+      source: function(request, response){
+        $.ajax({
+            type: 'get'
+          , url: ajaxUrl
+          , dataType: "json"
+          , data: {$opt : request.term}
+          , success: function(data) {
+            response( $.map( data, function( item ) {
+              var toShow = item.order_number + ' '+ item.id_number + ' '+ item.name + ' '+ item.email + ' '+ item.phone_number + ' '+ item.topping + ' '+ item.pay_method + ' '+ item.call_first + ' '+ item.order_date;
+              return {
+                label: toShow,
+                value: toShow
+              }
+            }));
+          }
+        });
+      },
+      minLength: 2
+    });
+  }).trigger( "change" );
 }());
