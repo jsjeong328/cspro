21bab98ab168b2daeea50e9daac0c994e399d403
jsjeong328 <jsjeong328@gmail.com>
2017-11-26 16:20:20 +0900

    test 17

--- public_html/searchAjax.js
+++ public_html/searchAjax.js
@@ -13,7 +13,7 @@ var runAutoComplete = (function(){
         +'<HR>';
     });
     $("#selectbox").html( str );
-    $("#"+$opt+"").autocomplete({
+    $("#order_number").autocomplete({
       source: function(request, response){
         // console.log($(this).text())
         // console.log($opt)
@@ -22,7 +22,191 @@ var runAutoComplete = (function(){
             type: 'get'
           , url: ajaxUrl
           , dataType: "json"
-          , data: {$opt : request.term}
+          , data: {"order_number" : request.term}
+          , success: function(data) {
+            response( $.map( data, function( item ) {
+              var toShow = item.order_number + ' '+ item.id_number + ' '+ item.name + ' '+ item.email + ' '+ item.phone_number + ' '+ item.topping + ' '+ item.pay_method + ' '+ item.call_first + ' '+ item.order_date;
+              return {
+                label: toShow,
+                value: toShow
+              }
+            }));
+          }
+        });
+      },
+      minLength: 2
+    });
+    $("#id_number").autocomplete({
+      source: function(request, response){
+        // console.log($(this).text())
+        // console.log($opt)
+        $opt.toString();
+        $.ajax({
+            type: 'get'
+          , url: ajaxUrl
+          , dataType: "json"
+          , data: {"id_number" : request.term}
+          , success: function(data) {
+            response( $.map( data, function( item ) {
+              var toShow = item.order_number + ' '+ item.id_number + ' '+ item.name + ' '+ item.email + ' '+ item.phone_number + ' '+ item.topping + ' '+ item.pay_method + ' '+ item.call_first + ' '+ item.order_date;
+              return {
+                label: toShow,
+                value: toShow
+              }
+            }));
+          }
+        });
+      },
+      minLength: 2
+    });
+    $("#name").autocomplete({
+      source: function(request, response){
+        // console.log($(this).text())
+        // console.log($opt)
+        $opt.toString();
+        $.ajax({
+            type: 'get'
+          , url: ajaxUrl
+          , dataType: "json"
+          , data: {"name" : request.term}
+          , success: function(data) {
+            response( $.map( data, function( item ) {
+              var toShow = item.order_number + ' '+ item.id_number + ' '+ item.name + ' '+ item.email + ' '+ item.phone_number + ' '+ item.topping + ' '+ item.pay_method + ' '+ item.call_first + ' '+ item.order_date;
+              return {
+                label: toShow,
+                value: toShow
+              }
+            }));
+          }
+        });
+      },
+      minLength: 2
+    });
+    $("#email").autocomplete({
+      source: function(request, response){
+        // console.log($(this).text())
+        // console.log($opt)
+        $opt.toString();
+        $.ajax({
+            type: 'get'
+          , url: ajaxUrl
+          , dataType: "json"
+          , data: {"email" : request.term}
+          , success: function(data) {
+            response( $.map( data, function( item ) {
+              var toShow = item.order_number + ' '+ item.id_number + ' '+ item.name + ' '+ item.email + ' '+ item.phone_number + ' '+ item.topping + ' '+ item.pay_method + ' '+ item.call_first + ' '+ item.order_date;
+              return {
+                label: toShow,
+                value: toShow
+              }
+            }));
+          }
+        });
+      },
+      minLength: 2
+    });
+    $("#phone_number").autocomplete({
+      source: function(request, response){
+        // console.log($(this).text())
+        // console.log($opt)
+        $opt.toString();
+        $.ajax({
+            type: 'get'
+          , url: ajaxUrl
+          , dataType: "json"
+          , data: {"phone_number" : request.term}
+          , success: function(data) {
+            response( $.map( data, function( item ) {
+              var toShow = item.order_number + ' '+ item.id_number + ' '+ item.name + ' '+ item.email + ' '+ item.phone_number + ' '+ item.topping + ' '+ item.pay_method + ' '+ item.call_first + ' '+ item.order_date;
+              return {
+                label: toShow,
+                value: toShow
+              }
+            }));
+          }
+        });
+      },
+      minLength: 2
+    });
+    $("#topping").autocomplete({
+      source: function(request, response){
+        // console.log($(this).text())
+        // console.log($opt)
+        $opt.toString();
+        $.ajax({
+            type: 'get'
+          , url: ajaxUrl
+          , dataType: "json"
+          , data: {"topping" : request.term}
+          , success: function(data) {
+            response( $.map( data, function( item ) {
+              var toShow = item.order_number + ' '+ item.id_number + ' '+ item.name + ' '+ item.email + ' '+ item.phone_number + ' '+ item.topping + ' '+ item.pay_method + ' '+ item.call_first + ' '+ item.order_date;
+              return {
+                label: toShow,
+                value: toShow
+              }
+            }));
+          }
+        });
+      },
+      minLength: 2
+    });
+    $("#pay_method").autocomplete({
+      source: function(request, response){
+        // console.log($(this).text())
+        // console.log($opt)
+        $opt.toString();
+        $.ajax({
+            type: 'get'
+          , url: ajaxUrl
+          , dataType: "json"
+          , data: {"pay_method" : request.term}
+          , success: function(data) {
+            response( $.map( data, function( item ) {
+              var toShow = item.order_number + ' '+ item.id_number + ' '+ item.name + ' '+ item.email + ' '+ item.phone_number + ' '+ item.topping + ' '+ item.pay_method + ' '+ item.call_first + ' '+ item.order_date;
+              return {
+                label: toShow,
+                value: toShow
+              }
+            }));
+          }
+        });
+      },
+      minLength: 2
+    });
+    $("#call_first").autocomplete({
+      source: function(request, response){
+        // console.log($(this).text())
+        // console.log($opt)
+        $opt.toString();
+        $.ajax({
+            type: 'get'
+          , url: ajaxUrl
+          , dataType: "json"
+          , data: {"call_first" : request.term}
+          , success: function(data) {
+            response( $.map( data, function( item ) {
+              var toShow = item.order_number + ' '+ item.id_number + ' '+ item.name + ' '+ item.email + ' '+ item.phone_number + ' '+ item.topping + ' '+ item.pay_method + ' '+ item.call_first + ' '+ item.order_date;
+              return {
+                label: toShow,
+                value: toShow
+              }
+            }));
+          }
+        });
+      },
+      minLength: 2
+    });
+    $("#order_date").autocomplete({
+      source: function(request, response){
+        // console.log($(this).text())
+        // console.log($opt)
+        $opt.toString();
+        $.ajax({
+            type: 'get'
+          , url: ajaxUrl
+          , dataType: "json"
+          , data: {"order_date" : request.term}
           , success: function(data) {
             response( $.map( data, function( item ) {
               var toShow = item.order_number + ' '+ item.id_number + ' '+ item.name + ' '+ item.email + ' '+ item.phone_number + ' '+ item.topping + ' '+ item.pay_method + ' '+ item.call_first + ' '+ item.order_date;
